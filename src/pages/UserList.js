b358619aab3f61871335600233099fe48d8203fc
Lucas de Lima <fionzera@gmail.com>
2023-06-27 11:29:55 -0300

    aula 08

--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,22 +1,52 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
+import './UserList.css';
+import axios from "axios";
 
 const UserList = () => {
-    const [state, setState] = useState(0);
+    const [userData, setUserData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    useEffect(() =>{
-        console.log('useEffect como didMount')
-    }, [state])
+    const fecthUserData = useCallback(async()=>{
+        try{
+            setLoading(true);
+            const {data} = await axios.get("https://jsonplaceholder.typicode.com/users")
+            console.log(data)
+        }catch(error){
+            console.error(error);
+        }finally{
+            setLoading(false);
+        }
+    },[])
 
-    const nomeFuncao = useCallback(()=>{
 
+    useEffect(() => {
+        fecthUserData();
     }, [])
 
-    const valor = useMemo(()=> {
-        
-    }, [])
+    const renderUserData = () =>{
+        if(loading){
+            return(
+                <div>
+                    <h3>Carregando</h3>
+                </div>
+            )
+        }else{
+            return <div>
+                <h2>Dados do usuário:</h2>
+                {userData.map(user => (
+                    <div>
+                        <h4>{user.name}</h4>
+                        <h5>{user.email}</h5>
+                        <span>{user.company.name}</span>
+                    </div>
+                ))}
+            </div>
+        }
+    }
+
     return (
         <div className="container">
-            <h1>Exemplo random</h1>
+            {renderUserData()}
         </div>
     );
 }
